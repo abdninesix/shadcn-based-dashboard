909010b9402c5b9fa2b9816f82918f9e8a2513b7
abdninesix <mohabd1995@gmail.com>
2025-04-26 23:09:43 +0500

    New

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from './ui/sidebar'
+import { Calendar, ChevronUpIcon, Home, Inbox, LogOut, Search, Settings, User2 } from 'lucide-react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+
+const AppSidebar = () => {
+
+  const items = [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Inbox",
+      url: "#",
+      icon: Inbox,
+    },
+    {
+      title: "Calender",
+      url: "#",
+      icon: Calendar,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ]
+
+  return (
+    <Sidebar collapsible='icon'>
+
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <Image src="/globe.svg" alt='Logo' height={20} width={20} />
+                <span>Shadcn Dashboard</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarSeparator/>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 />Abdullah<ChevronUpIcon className='ml-auto' />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem><User2 />Profile</DropdownMenuItem>
+                <DropdownMenuItem><Settings />Settings</DropdownMenuItem>
+                <DropdownMenuItem variant='destructive'><LogOut />Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+
+    </Sidebar>
+  )
+}
+
+export default AppSidebar
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { LogOut, LucideComputer, Moon, Settings, Sun, ToggleLeftIcon, User } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { Button } from './ui/button'
+import { useTheme } from 'next-themes'
+import { SidebarTrigger, useSidebar } from './ui/sidebar'
+
+const Navbar = () => {
+
+    const { setTheme } = useTheme()
+    const { toggleSidebar} = useSidebar()
+    return (
+        <nav className='flex items-center justify-between p-4'>
+            {/* Left */}
+            <SidebarTrigger/>
+            <Button variant="ghost" onClick={toggleSidebar}><ToggleLeftIcon/></Button>
+            {/* Right */}
+            <div className='flex items-center gap-4'>
+                <Link href="/">Dashboard</Link>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon">
+                            <Sun className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+                            <Moon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Toggle theme</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onClick={() => setTheme("light")}><Sun />Light</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}><Moon /> Dark</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}><LucideComputer /> System</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem><User />Profile</DropdownMenuItem>
+                        <DropdownMenuItem><Settings />Settings</DropdownMenuItem>
+                        <DropdownMenuItem variant='destructive'><LogOut />Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
