ef291ec3e6e273ff900ea77b4f84bdc401008463
abdninesix <mohabd1995@gmail.com>
2025-04-27 19:47:42 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AppAreachart from "@/components/AppAreachart";
 import AppBarchart from "@/components/AppBarchart";
+import AppPiechart from "@/components/AppPiechart";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -7,10 +8,10 @@ export default function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarchart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Object</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Object</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Object</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppPiechart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Object</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppAreachart/></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Object</div>
     </div>
   );
 }
